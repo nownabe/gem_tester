49517dc64d511dcae3295b82ae41d235c794f5df
nownabe <nownabe@gmail.com>
2017-09-24 21:30:13 +0900

    Refactor

--- scripts/test-all.rb
+++ scripts/test-all.rb
@@ -3,20 +3,138 @@ require "timeout"
 require "parallel"
 
 class Tester
-  IMAGES = %w(
-    centos-7
-    debian-8
-    debian-9
-    ubuntu-1404-lts
-    ubuntu-1604-lts
-    ubuntu-1704
-  ).freeze
-  IMAGES = %w(
-    centos-7
-    debian-8
-    ubuntu-1604-lts
-  ).freeze
   SCRIPT_URL = "https://raw.githubusercontent.com/nownabe/gem_tester/master/scripts/run-gem_tester.sh"
+
+  attr_reader :branch
+  attr_reader :enable_shared
+  attr_reader :image_family
+
+  def initialize(image_family, branch:, enable_shared:)
+    @image_family  = image_family
+    @branch        = branch
+    @enable_shared = enable_shared
+    @image_project = get_image_project
+  end
+
+  def run
+    create_instance
+    wait_instance
+    run_gem_tester
+  ensure
+    delete_instance
+  end
+
+  private
+
+  def create_instance
+    exec(%W[
+      gcloud compute instances create
+      #{instance_name}
+      --boot-disk-size 50GB
+      --machine-type n1-highcpu-8
+      --zone asia-northeast1-a
+      --image-project #{image_project}
+      --image-family #{image_family}
+    ])
+  end
+
+  def delete_instance
+    exec(%W[
+      yes |
+      gcloud compute instances delete
+      #{instance_name}
+      --delete-disks all
+      --zone asia-northeast1-a
+    ])
+  end
+
+  def enable_shared?
+    !!enable_shared
+  end
+
+  def exec(*cmd)
+    cmd = cmd.join(" ")
+    `echo >>#{log}`
+    `echo '$ #{cmd}' >>#{log}`
+    `#{cmd} >>#{log} 2>&1`
+    $?
+  end
+
+  def image_project
+    @image_project ||= `gcloud compute images list | grep #{image} | awk '{ print $2 }'`.chomp
+  end
+
+  def instance_name
+    @instance_name ||= "gem-tester-#{image_family}"
+  end
+
+  def log
+    @log ||= "logs/#{branch}#{enable_shared? ? "-enable-shared" : ""}-#{image_family}.log"
+  end
+
+  def run_gem_tester
+    ret = exec(%W[
+      gcloud compute ssh
+      #{instance_name}
+      --zone asia-northeast1-a
+      --command "#{test_command}"
+      -- -t
+    ])
+
+    if ret.success?
+      puts "#{image} - Succeeded!"
+    else
+      puts "#{image} - Failed!"
+    end
+  end
+
+  def script_url
+    SCRIPT_URL
+  end
+
+  def test_command
+    %W[
+      curl -LsS
+      #{script_url}
+      | BRANCH=#{branch}
+      CONFIGURE_OPTIONS=#{enable_shared? ? "--enable-shared" : ""}
+      bash
+    ].join(" ")
+  end
+
+  def wait_instance
+    Timeout.timeout(120) do
+      loop do
+        ret = exec(%W[
+          gcloud compute ssh
+          #{instance_name}
+          --zone asia-northeast1-a
+          --command "hostname"
+        ])
+        break if ret.success?
+        sleep 5
+      end
+    end
+  rescue Timeout::Error
+    puts "#{image_family} - Failed! (instances didn't start)"
+    raise Parallel::Break
+  end
+end
+
+class ParallelTester
+  # IMAGES = %w(
+  #   centos-7
+  #   debian-8
+  #   debian-9
+  #   ubuntu-1404-lts
+  #   ubuntu-1604-lts
+  #   ubuntu-1704
+  # ).freeze
+  IMAGES = %w(
+    centos-7
+    debian-8
+    ubuntu-1604-lts
+  ).freeze
 
   attr_reader :branch
   attr_reader :enable_shared
@@ -29,93 +147,18 @@ class Tester
   def run
     puts "* #{branch} / enable_shared: #{enable_shared} (#{log})"
     Parallel.each(images, in_threads: images.size) do |image|
-      image_project = `gcloud compute images list | grep #{image} | awk '{ print $2 }'`.chomp
-
-      cmd = %W[
-        gcloud compute instances create
-        gem-tester-#{image}
-        --boot-disk-size 50GB
-        --machine-type n1-highcpu-8
-        --zone asia-northeast1-a
-        --image-project #{image_project}
-        --image-family #{image}
-      ].join(" ")
-      exec(cmd)
-
-      begin
-        Timeout.timeout(120) do
-          loop do
-            cmd = "gcloud compute ssh gem-tester-#{image} --zone asia-northeast1-a --command \"hostname\""
-            ret = exec(cmd)
-            break if ret.success?
-            sleep 5
-          end
-        end
-      rescue Timeout::Error
-        puts "#{image} - Failed! (instances didn't start)"
-        raise Parallel::Break
-      end
-
-      test_command = %W[
-        curl -LsS
-        #{SCRIPT_URL}
-        | BRANCH=#{branch}
-        CONFIGURE_OPTIONS=#{enable_shared? ? "--enable-shared" : ""}
-        bash
-      ].join(" ")
-
-      cmd = %W[
-        gcloud compute ssh
-        gem-tester-#{image}
-        --zone asia-northeast1-a
-        --command "#{test_command}"
-        -- -t
-      ].join(" ")
-
-      ret = exec(cmd)
-
-      if ret.success?
-        puts "#{image} - Succeeded!"
-      else
-        puts "#{image} - Failed!"
-      end
-
-      cmd = %W[
-        yes |
-        gcloud compute instances delete
-        gem-tester-#{image}
-        --delete-disks all
-        --zone asia-northeast1-a
-      ].join(" ")
-
-      exec(cmd)
+      Tester.new(image, branch: branch, enable_shared: enable_shared).run
     end
   end
 
   private
 
-  def enable_shared?
-    !!enable_shared
-  end
-
-  def exec(cmd)
-    `echo >>#{log}`
-    `echo '$ #{cmd}' >>#{log}`
-    `#{cmd} >>#{log} 2>&1`
-    $?
-  end
-
   def images
-    # IMAGES
-    ["ubuntu-1604-lts"]
-  end
-
-  def log
-    @log ||= "logs/#{branch}#{enable_shared? ? "-enable-shared" : ""}.log"
+    IMAGES
   end
 end
 
-Tester.new(branch: "gem_tester-trunk", enable_shared: true).run
-Tester.new(branch: "gem_tester-trunk", enable_shared: false).run
-Tester.new(branch: "gem_tester-2.4.2", enable_shared: true).run
-Tester.new(branch: "gem_tester-2.4.2", enable_shared: false).run
+ParallelTester.new(branch: "gem_tester-trunk", enable_shared: true).run
+ParallelTester.new(branch: "gem_tester-trunk", enable_shared: false).run
+ParallelTester.new(branch: "gem_tester-2.4.2", enable_shared: true).run
+ParallelTester.new(branch: "gem_tester-2.4.2", enable_shared: false).run
